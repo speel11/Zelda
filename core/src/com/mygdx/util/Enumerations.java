ef9aae4a06fbd1cce7e8e8e7f23deac9e873416a
Sean Peel <Sean Peel@DESKTOP-E2VRB08>
2017-02-08 13:07:13 -0500

    Added Enumeration class

---
+++ core/src/com/mygdx/util/Enumerations.java
@@ -0,0 +1,16 @@
+package com.mygdx.util;
+
+/**
+ * @author Sean Peel
+ */
+
+public class Enumerations {
+    
+    public static enum Direction {
+        UP, RIGHT, DOWN, LEFT
+    };
+    
+    private static enum State {
+
+    };
+}
