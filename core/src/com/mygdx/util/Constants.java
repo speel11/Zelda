220844cbda6956548c043291fedc75549a8a9800
Sean Peel <Sean Peel@DESKTOP-E2VRB08>
2017-02-08 14:16:32 -0500

    Added more constants.

--- core/src/com/mygdx/util/Constants.java
+++ core/src/com/mygdx/util/Constants.java
@@ -3,11 +3,24 @@ package com.mygdx.util;
 /**
  * @author Sean Peel
  */
-
 public class Constants {
+
+    //public static final int SCALE = 4;
+    public static final int SPRITE_SIZE = 16;
+    
+    public static final float LINK_WALKING_SPEED = 150.0f;
+    
     public static final String LINK_WALKING_UP_ASSETS_ID = "link_walk_up";
     public static final String LINK_WALKING_DOWN_ASSETS_ID = "link_walk_down";
-    public static final String LINK_WALKING_SIDE_ASSETS_ID = "link_walk_side";
+    public static final String LINK_WALKING_LEFT_ASSETS_ID = "link_walk_left";
+    public static final String LINK_WALKING_RIGHT_ASSETS_ID = "link_walk_right";
+
+    public static final String LINK_WALKING_UP_ASSETS_PATH = "link_walk_up.png";
+    public static final String LINK_WALKING_DOWN_ASSETS_PATH = "link_walk_down.png";
+    public static final String LINK_WALKING_SIDE_ASSETS_PATH = "link_walk_side.png";
     
+    public static final int LINK_WALKING_COLS = 2;
+    public static final int LINK_WALKING_ROWS = 1;
     
+    public static final float ANIM_SPEED = 0.1f;
 }
