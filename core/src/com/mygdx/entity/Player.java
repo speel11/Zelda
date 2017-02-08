3086bed78d1534aebd71cc5799072c7cc27458ef
Sean Peel <Sean Peel@DESKTOP-E2VRB08>
2017-02-08 16:54:02 -0500

    Added player states, attacking animations, and a couple helper methods.

--- core/src/com/mygdx/entity/Player.java
+++ core/src/com/mygdx/entity/Player.java
@@ -8,57 +8,111 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.mygdx.util.AssetManager;
 import com.mygdx.util.Constants;
 import com.mygdx.util.Enumerations.Direction;
-import java.util.InputMismatchException;
+import com.mygdx.util.Enumerations.LinkStates;
 
 /**
  * @author Sean Peel
  */
 public class Player {
 
-    private Animation walkingAnim;
-    private Direction dir;
+    private Animation animation;
+    private Direction facing;
+    private LinkStates state;
     private float stateTime;
-    private float playerX = 100;
-    private float playerY = 100;
-    private float playerSpeed = Constants.LINK_WALKING_SPEED;
+    private float playerX;
+    private float playerY;
+    private float playerSpeed;
 
     public Player() {
-        dir = Direction.UP;
+        playerX = 100;
+        playerY = 100;
+        playerSpeed = Constants.LINK_WALKING_SPEED;
+        facing = Direction.UP;
+        state = LinkStates.MOVING;
     }
 
     private void checkInput() {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            dir = Direction.LEFT;
-            stateTime += Gdx.graphics.getDeltaTime();
-            playerX -= playerSpeed * Gdx.graphics.getDeltaTime();
+            movePlayer(Direction.LEFT);
+
         } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            dir = Direction.RIGHT;
-            stateTime += Gdx.graphics.getDeltaTime();
-            playerX += playerSpeed * Gdx.graphics.getDeltaTime();
+            movePlayer(Direction.RIGHT);
+
         } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            dir = Direction.DOWN;
-            stateTime += Gdx.graphics.getDeltaTime();
-            playerY -= playerSpeed * Gdx.graphics.getDeltaTime();
+            movePlayer(Direction.DOWN);
+
         } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            dir = Direction.UP;
-            stateTime += Gdx.graphics.getDeltaTime();
-            playerY += playerSpeed * Gdx.graphics.getDeltaTime();
+            movePlayer(Direction.UP);
+
+        } else {
+            state = LinkStates.IDLE;
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-            stateTime = 0;
-            playerSpeed = 0;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            state = LinkStates.ATTACK_START;
+
         }
 
-        setWalkingAnimation(dir);
+        setAnimation();
+    }
+
+    private void checkStates(SpriteBatch batch) {
+        switch (state) {
+            case IDLE:
+                checkInput();
+                break;
+            case MOVING:
+                checkInput();
+                break;
+            case ATTACK_START:
+                stateTime = 0.25f;
+                playerSpeed = 0;
+                state = LinkStates.ATTACK_SWORD;
+                break;
+            case ATTACK_SWORD:
+                if (stateTime <= 0.18f) {
+                    drawSword(batch);
+                }
+                if (stateTime > 0) {
+                    stateTime -= Gdx.graphics.getDeltaTime();
+                } else {
+                    state = LinkStates.IDLE;
+                }
+                break;
+
+        }
+    }
+
+    private void movePlayer(Direction dir) {
+        state = LinkStates.MOVING;
+        facing = dir;
+        stateTime += Gdx.graphics.getDeltaTime();
+
+        switch (dir) {
+            case UP:
+                playerY += playerSpeed * Gdx.graphics.getDeltaTime();
+                break;
+            case RIGHT:
+                playerX += playerSpeed * Gdx.graphics.getDeltaTime();
+                break;
+            case DOWN:
+                playerY -= playerSpeed * Gdx.graphics.getDeltaTime();
+                break;
+            case LEFT:
+                playerX -= playerSpeed * Gdx.graphics.getDeltaTime();
+                break;
+            default:
+                break;
+        }
+
     }
 
     public void render(SpriteBatch batch) {
-        checkInput();
-
-        TextureRegion currentFrame = walkingAnim.getKeyFrame(stateTime, true);
-
         batch.begin();
+        checkStates(batch);
+
+        TextureRegion currentFrame = animation.getKeyFrame(stateTime, true);
+
         batch.draw(currentFrame, playerX, playerY, Constants.SPRITE_SIZE, Constants.SPRITE_SIZE);
         batch.end();
     }
@@ -67,24 +121,106 @@ public class Player {
 
     }
 
-    private void setWalkingAnimation(Direction dir) {
-        playerSpeed = Constants.LINK_WALKING_SPEED;
+    private void drawSword(SpriteBatch batch) {
+        TextureRegion sword = AssetManager.getTexture(Constants.SWORD_ASSETS_ID);
 
-        switch (dir) {
+        switch (facing) {
             case UP:
-                walkingAnim = AssetManager.getAnimation(Constants.LINK_WALKING_UP_ASSETS_ID);
-                break;
-            case RIGHT:
-                walkingAnim = AssetManager.getAnimation(Constants.LINK_WALKING_RIGHT_ASSETS_ID);
-                break;
-            case DOWN:
-                walkingAnim = AssetManager.getAnimation(Constants.LINK_WALKING_DOWN_ASSETS_ID);
+                batch.draw(sword, playerX, playerY + 12,
+                        Constants.SWORD_ROTATION_X, Constants.SWORD_ROTATION_Y,
+                        Constants.SPRITE_SIZE, Constants.SPRITE_SIZE,
+                        Constants.SCALE, Constants.SCALE,
+                        Constants.SWORD_ROTATION_UP);
                 break;
             case LEFT:
-                walkingAnim = AssetManager.getAnimation(Constants.LINK_WALKING_LEFT_ASSETS_ID);
+                batch.draw(sword, playerX, playerY,
+                        Constants.SWORD_ROTATION_X, Constants.SWORD_ROTATION_Y,
+                        Constants.SPRITE_SIZE, Constants.SPRITE_SIZE,
+                        Constants.SCALE, Constants.SCALE,
+                        Constants.SWORD_ROTATION_LEFT);
                 break;
+            case DOWN:
+                batch.draw(sword, playerX + 12, playerY,
+                        Constants.SWORD_ROTATION_X, Constants.SWORD_ROTATION_Y,
+                        Constants.SPRITE_SIZE, Constants.SPRITE_SIZE,
+                        Constants.SCALE, Constants.SCALE,
+                        Constants.SWORD_ROTATION_DOWN);
+                break;
+            case RIGHT:
+                batch.draw(sword, playerX + 12, playerY + 12,
+                        Constants.SWORD_ROTATION_X, Constants.SWORD_ROTATION_Y,
+                        Constants.SPRITE_SIZE, Constants.SPRITE_SIZE,
+                        Constants.SCALE, Constants.SCALE,
+                        Constants.SWORD_ROTATION_RIGHT);
+                break;
+        }
+
+    }
+
+    private void setAnimation() {
+        switch (state) {
+            case IDLE:
+                if (facing != null) {
+                    switch (facing) {
+                        case UP:
+                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_UP_ASSETS_ID);
+                            break;
+                        case RIGHT:
+                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_RIGHT_ASSETS_ID);
+                            break;
+                        case DOWN:
+                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_DOWN_ASSETS_ID);
+                            break;
+                        case LEFT:
+                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_LEFT_ASSETS_ID);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                break;
+
+            case MOVING:
+                playerSpeed = Constants.LINK_WALKING_SPEED;
+                if (facing != null) {
+                    switch (facing) {
+                        case UP:
+                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_UP_ASSETS_ID);
+                            break;
+                        case RIGHT:
+                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_RIGHT_ASSETS_ID);
+                            break;
+                        case DOWN:
+                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_DOWN_ASSETS_ID);
+                            break;
+                        case LEFT:
+                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_LEFT_ASSETS_ID);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                break;
+            case ATTACK_START:
+                if (facing != null) {
+                    switch (facing) {
+                        case UP:
+                            animation = AssetManager.getAnimation(Constants.LINK_ATTACKING_UP_ASSETS_ID);
+                            break;
+                        case RIGHT:
+                            animation = AssetManager.getAnimation(Constants.LINK_ATTACKING_RIGHT_ASSETS_ID);
+                            break;
+                        case DOWN:
+                            animation = AssetManager.getAnimation(Constants.LINK_ATTACKING_DOWN_ASSETS_ID);
+                            break;
+                        case LEFT:
+                            animation = AssetManager.getAnimation(Constants.LINK_ATTACKING_LEFT_ASSETS_ID);
+                        default:
+                            break;
+                    }
+                }
             default:
-                throw new InputMismatchException("Direction must be up, down, right, or left.");
+                break;
         }
 
     }
