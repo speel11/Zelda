f814373de483045b119b589990e385217905d33e
Sean Peel <Sean Peel@DESKTOP-E2VRB08>
2017-02-08 16:54:31 -0500

    Added more constants.

--- core/src/com/mygdx/util/Constants.java
+++ core/src/com/mygdx/util/Constants.java
@@ -5,22 +5,45 @@ package com.mygdx.util;
  */
 public class Constants {
 
-    //public static final int SCALE = 4;
+    public static final int SCALE = 1;
     public static final int SPRITE_SIZE = 16;
-    
-    public static final float LINK_WALKING_SPEED = 150.0f;
-    
+    public static final float LINK_WALKING_SPEED = 100.0f;
+
+    public static final int SWORD_ROTATION_X = 2;
+    public static final int SWORD_ROTATION_Y = 2;
+
+    public static final int SWORD_ROTATION_UP = 0;
+    public static final int SWORD_ROTATION_LEFT = 90;
+    public static final int SWORD_ROTATION_DOWN = 180;
+    public static final int SWORD_ROTATION_RIGHT = 270;
+
     public static final String LINK_WALKING_UP_ASSETS_ID = "link_walk_up";
     public static final String LINK_WALKING_DOWN_ASSETS_ID = "link_walk_down";
     public static final String LINK_WALKING_LEFT_ASSETS_ID = "link_walk_left";
     public static final String LINK_WALKING_RIGHT_ASSETS_ID = "link_walk_right";
 
+    public static final String LINK_ATTACKING_UP_ASSETS_ID = "link_attack_up";
+    public static final String LINK_ATTACKING_DOWN_ASSETS_ID = "link_attack_down";
+    public static final String LINK_ATTACKING_LEFT_ASSETS_ID = "link_attack_left";
+    public static final String LINK_ATTACKING_RIGHT_ASSETS_ID = "link_attack_right";
+
+    public static final String SWORD_ASSETS_ID = "link_sword";
+
     public static final String LINK_WALKING_UP_ASSETS_PATH = "link_walk_up.png";
     public static final String LINK_WALKING_DOWN_ASSETS_PATH = "link_walk_down.png";
     public static final String LINK_WALKING_SIDE_ASSETS_PATH = "link_walk_side.png";
-    
+
+    public static final String LINK_ATTACKING_UP_ASSETS_PATH = "link_attack_up.png";
+    public static final String LINK_ATTACKING_DOWN_ASSETS_PATH = "link_attack_down.png";
+    public static final String LINK_ATTACKING_SIDE_ASSETS_PATH = "link_attack_side.png";
+
+    public static final String SWORD_ASSETS_PATH = "link_sword.png";
+
     public static final int LINK_WALKING_COLS = 2;
     public static final int LINK_WALKING_ROWS = 1;
-    
+
+    public static final int LINK_ATTACKING_COLS = 1;
+    public static final int LINK_ATTACKING_ROWS = 1;
+
     public static final float ANIM_SPEED = 0.1f;
 }
