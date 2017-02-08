167af7c528fb33153c647eb4bc758a2c939f4adc
Sean Peel <Sean Peel@DESKTOP-E2VRB08>
2017-02-08 16:54:20 -0500

    Added more assets.

--- core/src/com/mygdx/util/AssetManager.java
+++ core/src/com/mygdx/util/AssetManager.java
@@ -14,7 +14,11 @@ public class AssetManager {
     private static HashMap<String, Animation> animationsMap = new HashMap<String, Animation>();
 
     public static void loadAssets() {
-
+        
+        texturesMap.put(Constants.SWORD_ASSETS_ID, new TextureRegion(
+                new Texture(Constants.SWORD_ASSETS_PATH)));
+        
+        
         animationsMap.put(Constants.LINK_WALKING_UP_ASSETS_ID,
                 createAnimation(new Texture(Constants.LINK_WALKING_UP_ASSETS_PATH),
                         Constants.LINK_WALKING_ROWS, Constants.LINK_WALKING_COLS,
@@ -30,6 +34,23 @@ public class AssetManager {
         animationsMap.put(Constants.LINK_WALKING_LEFT_ASSETS_ID,
                 createAnimation(new Texture(Constants.LINK_WALKING_SIDE_ASSETS_PATH),
                         Constants.LINK_WALKING_ROWS, Constants.LINK_WALKING_COLS,
+                        false, false));
+
+        animationsMap.put(Constants.LINK_ATTACKING_UP_ASSETS_ID,
+                createAnimation(new Texture(Constants.LINK_ATTACKING_UP_ASSETS_PATH),
+                        Constants.LINK_ATTACKING_ROWS, Constants.LINK_ATTACKING_COLS,
+                        false, false));
+        animationsMap.put(Constants.LINK_ATTACKING_RIGHT_ASSETS_ID,
+                createAnimation(new Texture(Constants.LINK_ATTACKING_SIDE_ASSETS_PATH),
+                        Constants.LINK_ATTACKING_ROWS, Constants.LINK_ATTACKING_COLS,
+                        true, false));
+        animationsMap.put(Constants.LINK_ATTACKING_DOWN_ASSETS_ID,
+                createAnimation(new Texture(Constants.LINK_ATTACKING_DOWN_ASSETS_PATH),
+                        Constants.LINK_ATTACKING_ROWS, Constants.LINK_ATTACKING_COLS,
+                        false, false));
+        animationsMap.put(Constants.LINK_ATTACKING_LEFT_ASSETS_ID,
+                createAnimation(new Texture(Constants.LINK_ATTACKING_SIDE_ASSETS_PATH),
+                        Constants.LINK_ATTACKING_ROWS, Constants.LINK_ATTACKING_COLS,
                         false, false));
     }
 
