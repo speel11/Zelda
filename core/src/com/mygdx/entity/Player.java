ce2e526db78dd7b2cd53867068f0d6900e86addc
Sean Peel <speel@oswego.edu>
2017-02-15 16:08:52 -0500

    Added collision detection

--- core/src/com/mygdx/entity/Player.java
+++ core/src/com/mygdx/entity/Player.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.mygdx.util.AssetManager;
 import com.mygdx.util.Constants;
 import com.mygdx.util.Enumerations.Direction;
@@ -15,6 +17,7 @@ import com.mygdx.util.Enumerations.LinkStates;
  */
 public class Player {
 
+    private TiledMapTileLayer collisionLayer;
     private Animation animation;
     private Direction facing;
     private LinkStates state;
@@ -23,7 +26,9 @@ public class Player {
     private float playerY;
     private float playerSpeed;
 
-    public Player() {
+    public Player(TiledMapTileLayer collisionLayer) {
+        this.collisionLayer = collisionLayer;
+
         playerX = 100;
         playerY = 100;
         playerSpeed = Constants.LINK_WALKING_SPEED;
@@ -59,8 +64,6 @@ public class Player {
     private void checkStates(SpriteBatch batch) {
         switch (state) {
             case IDLE:
-                checkInput();
-                break;
             case MOVING:
                 checkInput();
                 break;
@@ -88,6 +91,7 @@ public class Player {
         facing = dir;
         stateTime += Gdx.graphics.getDeltaTime();
 
+        checkCollisions();
         switch (dir) {
             case UP:
                 playerY += playerSpeed * Gdx.graphics.getDeltaTime();
@@ -119,6 +123,85 @@ public class Player {
 
     public void dispose() {
 
+    }
+
+    private void checkCollisions() {
+
+        if (isCellBlocked(playerX, playerY) || isCellHalfBlocked(playerX, playerY)) {
+            playerSpeed = 0;
+        } else {
+            playerSpeed = Constants.LINK_WALKING_SPEED;
+        }
+    }
+
+    private boolean isCellBlocked(float x, float y) {
+        Cell cell = null;
+        boolean blocked = false;
+
+        switch (facing) {
+            case UP:
+                cell = collisionLayer.getCell((int) ((x + 8) / collisionLayer.getTileWidth()),
+                        (int) ((y) / collisionLayer.getTileHeight()) + 1);
+                break;
+            case RIGHT:
+                cell = collisionLayer.getCell((int) ((x - 1) / collisionLayer.getTileWidth() + 1),
+                        (int) ((y + 8) / collisionLayer.getTileHeight()));
+                break;
+            case DOWN:
+                cell = collisionLayer.getCell((int) ((x + 8) / collisionLayer.getTileWidth()),
+                        (int) ((y) / collisionLayer.getTileHeight()));
+                break;
+            case LEFT:
+                cell = collisionLayer.getCell((int) ((x + 1) / collisionLayer.getTileWidth()),
+                        (int) ((y + 8) / collisionLayer.getTileHeight()));
+                break;
+        }
+
+        if (cell != null && cell.getTile() != null) {
+            blocked = cell.getTile().getProperties().containsKey("blocked");
+        }
+
+        return blocked;
+    }
+
+    private boolean isCellHalfBlocked(float x, float y) {
+        Cell leftCell = null;
+        Cell rightCell = null;
+
+        boolean leftBlocked = false;
+        boolean rightBlocked = false;
+
+        switch (facing) {
+            case UP:
+                leftCell = collisionLayer.getCell((int) ((x + 3) / collisionLayer.getTileWidth()),
+                        (int) ((y + 8) / collisionLayer.getTileHeight()));
+                rightCell = collisionLayer.getCell((int) ((x + 14) / collisionLayer.getTileWidth()),
+                        (int) ((y + 8) / collisionLayer.getTileHeight()));
+                break;
+            case RIGHT:
+                leftCell = collisionLayer.getCell((int) ((x + 16) / collisionLayer.getTileWidth()),
+                        (int) ((y+4) / collisionLayer.getTileHeight()));
+                break;
+            case DOWN:
+                leftCell = collisionLayer.getCell((int) ((x + 3) / collisionLayer.getTileWidth()),
+                        (int) ((y-1) / collisionLayer.getTileHeight()));
+                rightCell = collisionLayer.getCell((int) ((x + 14) / collisionLayer.getTileWidth()),
+                        (int) ((y-1) / collisionLayer.getTileHeight()));
+                break;
+            case LEFT:
+                leftCell = collisionLayer.getCell((int) ((x + 1) / collisionLayer.getTileWidth()),
+                        (int) ((y+4) / collisionLayer.getTileHeight()));
+                break;
+        }
+
+        if (leftCell != null && leftCell.getTile() != null) {
+            leftBlocked = leftCell.getTile().getProperties().containsKey("halfblocked");
+        }
+        if (rightCell != null && rightCell.getTile() != null) {
+            rightBlocked = rightCell.getTile().getProperties().containsKey("halfblocked");
+        }
+
+        return leftBlocked || rightBlocked;
     }
 
     private void drawSword(SpriteBatch batch) {
@@ -160,26 +243,7 @@ public class Player {
     private void setAnimation() {
         switch (state) {
             case IDLE:
-                if (facing != null) {
-                    switch (facing) {
-                        case UP:
-                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_UP_ASSETS_ID);
-                            break;
-                        case RIGHT:
-                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_RIGHT_ASSETS_ID);
-                            break;
-                        case DOWN:
-                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_DOWN_ASSETS_ID);
-                            break;
-                        case LEFT:
-                            animation = AssetManager.getAnimation(Constants.LINK_WALKING_LEFT_ASSETS_ID);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                break;
-
+            //WHEN IDLE - GET THE SAME ANIMS FOR MOVING
             case MOVING:
                 playerSpeed = Constants.LINK_WALKING_SPEED;
                 if (facing != null) {
